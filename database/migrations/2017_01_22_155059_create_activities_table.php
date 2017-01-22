2ed9b124c2f1fb4fdf023e38a5510760043c3f66
Chin-Wen Tsay <cwtsai@nfu.edu.tw>
2017-01-23 00:52:57 +0800

    migrate

--- database/migrations/2017_01_22_155059_create_activities_table.php
+++ database/migrations/2017_01_22_155059_create_activities_table.php
@@ -16,6 +16,12 @@ class CreateActivitiesTable extends Migration
         Schema::create('activities', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+             $table->string('act_no', 6)->nullable();       //活動編號
+            $table->string('act_name', 60)->nullable();  //活動名稱
+            $table->date('act_beg_date')->nullable();  //活動開始日期
+            $table->date('act_end_date')->nullable();      //活動結束日期
+        
+
         });
     }
 
