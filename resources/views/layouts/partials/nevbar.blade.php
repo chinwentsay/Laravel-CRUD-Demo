83c69b50ae13ffc96435ae45516d351e4628fd9b
Chin-Wen Tsay <cwtsai@nfu.edu.tw>
2017-01-22 22:24:37 +0800

    修改選單的文字 layouts/partial/nevbar.blade.php

--- resources/views/layouts/partials/nevbar.blade.php
+++ resources/views/layouts/partials/nevbar.blade.php
@@ -1,5 +1,5 @@
-<a href="https://laravel.com/docs">Documentation</a>
- <a href="https://laracasts.com">Laracasts</a>
+<a href="https://laravel.com/docs">最新活動</a>
+ <a href="https://laracasts.com">後台管理</a>
  <a href="https://laravel-news.com">News</a>
  <a href="https://forge.laravel.com">Forge</a>
  <a href="https://github.com/laravel/laravel">GitHub</a>
