415e6a17cd7098dab643ee99ad0adf9fae525a0b
Chin-Wen Tsay <cwtsai@nfu.edu.tw>
2017-01-23 00:48:18 +0800

    建立 migrate 內容

--- app/Activity.php
+++ app/Activity.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
-    //
+    // table name
+   protected $table = 'activities';
+   
+   /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    	'act_no',
+    	'act_name',
+    	'act_beg_date',
+    	'act_end_date',
+    ];
+    
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        //
+    ];
 }
