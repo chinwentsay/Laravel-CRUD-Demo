a214369e431a2c8e0b75b1076e743ad5951bb5d4
Chin-Wen Tsay <cwtsai@nfu.edu.tw>
2017-01-22 22:02:51 +0800

    新增 footer, 將 menu 拆到 layouts/partials/nevbar.blade.php

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer style="padding:0 0 0 0;">
+<font color="#ffffff">
+      國立虎尾科技大學National Formosa University © All rights reserved. <br>
+      電話：886-5-6315000    傳真：886-5-6315999    統一編號：64967512 <br>
+      地址：632 雲林縣虎尾鎮文化路64號 No.64, Wunhua Rd., Huwei Township, Yunlin County 632, Taiwan
+    </font>
+</footer>
