061a4ffc8f5e5c96583f9998676021477fa59dd5
Chin-Wen Tsay <cwtsai@nfu.edu.tw>
2017-01-23 14:52:00 +0800

    掛載到首頁選單

--- resources/views/layouts/partials/nevbar.blade.php
+++ resources/views/layouts/partials/nevbar.blade.php
@@ -1,4 +1,4 @@
-<a href="https://laravel.com/docs">最新活動</a>
+<a href=" {{ route('activityController.index') }} ">最新活動</a>
  <a href="https://laracasts.com">後台管理</a>
  <a href="https://laravel-news.com">News</a>
  <a href="https://forge.laravel.com">Forge</a>
