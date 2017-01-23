03c5c0178a89fab8d771f7b6d1725a5a8773370e
Chin-Wen Tsay <cwtsai@nfu.edu.tw>
2017-01-23 21:54:48 +0800

    調整首頁選單 views/layouts/partials/nevbar.blade.php 設定後台管理入口

--- resources/views/layouts/partials/nevbar.blade.php
+++ resources/views/layouts/partials/nevbar.blade.php
@@ -1,5 +1,6 @@
-<a href=" {{ route('activityController.index') }} ">最新活動</a>
- <a href="https://laracasts.com">後台管理</a>
+ <a href="https://laracasts.com">最新消息</a>
+<a href=" {{ route('activityController.index') }} ">後台管理</a>
+
  <a href="https://laravel-news.com">News</a>
  <a href="https://forge.laravel.com">Forge</a>
  <a href="https://github.com/laravel/laravel">GitHub</a>
