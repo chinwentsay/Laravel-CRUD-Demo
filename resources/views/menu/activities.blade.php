1590be07db7317525504c15e084a184785deaee4
Chin-Wen Tsay <cwtsai@nfu.edu.tw>
2017-01-23 14:32:30 +0800

    將 act_beg_date, act_end_date 顯示

--- resources/views/menu/activities.blade.php
+++ resources/views/menu/activities.blade.php
@@ -6,12 +6,15 @@
         <tr>
         <th>活動編號</th>
         <th>活動主旨</th>
-     
+        <th>活動開始日期</th>
+        <th>活動結束日期</th>
         </tr>
  @foreach($data  as $act)
       <tr>
       <td> {{ $act->act_no}} </td>
       <td> {{ $act->act_name}} </td>
+        <td> {{ $act->act_beg_date }} </td>
+        <td> {{  $act->act_end_date }} </td>
       </tr>
  @endforeach
 
