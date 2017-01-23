a6f78e8f48702a4cd6d83b03a4cb00194616b0c2
Chin-Wen Tsay <cwtsai@nfu.edu.tw>
2017-01-23 14:05:32 +0800

    一次產生 20 個測試資料 artisan db:seed

--- database/seeds/ActivityTableSeeder.php
+++ database/seeds/ActivityTableSeeder.php
@@ -16,15 +16,24 @@ class ActivityTableSeeder extends Seeder
     public function run()
     {
         //
-        //DB::table('activities') -> delete();
-      Activity::create([
+      DB::table('activities') -> delete();
+      /*
+         Activity::create([
         		'act_no' => 'act01',
         		'act_name' => 'titel',
         		'act_beg_date' => Carbon::now(),
         	 	'act_end_date' => Carbon::now()	,	
         		]);
-      
-        	
+      */
+      $a = range(1,20);
+      foreach ($a as $i) {
+         Activity::create([
+                'act_no' => 'act' . $i,
+                'act_name' => str_random(8),
+        	    'act_beg_date' => Carbon::now()->subDay($i),
+                'act_end_date' => Carbon::now()->addDay($i) ,   
+            ]);
        
-    }
+        }
+      }
 }
